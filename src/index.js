92cee96a4e2d67f25a0b2799ea85ba5a0aa4a446
Tamas Peter <me@example.com>
2020-05-17 13:05:29 +0200

    feat: Add basic React Router functionality
     - Route and link to '/' and '/search' pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+   // Wrap the App inside BrowserRouter to be able to use React Router functions
+   <BrowserRouter>
+      <App />
+   </BrowserRouter>, 
+   document.getElementById('root'));
